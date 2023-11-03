ee64eafa317283e4b466a4c0b2475387301b6b7e
vivianoranges <137729669+vivianoranges@users.noreply.github.com>
2023-11-03 15:07:37 +1100

    50% finished
    hopefully

--- Assets/Audio Clips/Audio.cs
+++ Assets/Audio Clips/Audio.cs
@@ -1,39 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Audio : MonoBehaviour
 {
     public AudioClip IntroBGM;
     public AudioClip NormalStateBGM;
 
+    private bool inTheIntro = true;
     private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        PlayIntro();
+
+        audioSource.clip = IntroBGM;
+        audioSource.loop = true;
+        audioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!audioSource.isPlaying)
+        if (SceneManager.GetActiveScene().name == "SampleScene" && inTheIntro)
         {
             NormalState();
         }
     }
 
-    private void PlayIntro()
-    {
-        audioSource.clip = IntroBGM;
-        audioSource.Play();
-    }
 
     private void NormalState()
     {
         audioSource.clip = NormalStateBGM;
         audioSource.Play();
+        inTheIntro = false;
     }
 }
